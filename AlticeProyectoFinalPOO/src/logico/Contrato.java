753a1d1ea51a14d2ca9458a746f590a46a10df1b
Alan Pacheco <alanpacheco1328@gmail.com>
2026-03-26 11:36:24 -0400

    Agregando constructores y getters and setters de Contrato y Pagos

--- AlticeProyectoFinalPOO/src/logico/Contrato.java
+++ AlticeProyectoFinalPOO/src/logico/Contrato.java
@@ -10,4 +10,53 @@ public class Contrato {
 	private LocalDate fechaFinContrato;
 	private boolean isActivo;
 	private Plan planContrato;
+	
+	public Contrato(String idContrato, Cliente cliente, LocalDate fechaInicioContrato, LocalDate fechaFinContrato, Plan planContrato) {
+		super();
+		this.idContrato = idContrato;
+		this.cliente = cliente;
+		this.fechaInicioContrato = fechaInicioContrato;
+		this.fechaFinContrato = fechaFinContrato;
+		this.isActivo = false;
+		this.planContrato = planContrato;
+	}
+	
+	public String getIdContrato() {
+		return idContrato;
+	}
+	public void setIdContrato(String idContrato) {
+		this.idContrato = idContrato;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public LocalDate getFechaInicioContrato() {
+		return fechaInicioContrato;
+	}
+	public void setFechaInicioContrato(LocalDate fechaInicioContrato) {
+		this.fechaInicioContrato = fechaInicioContrato;
+	}
+	public LocalDate getFechaFinContrato() {
+		return fechaFinContrato;
+	}
+	public void setFechaFinContrato(LocalDate fechaFinContrato) {
+		this.fechaFinContrato = fechaFinContrato;
+	}
+	public boolean isActivo() {
+		return isActivo;
+	}
+	public void setActivo(boolean isActivo) {
+		this.isActivo = isActivo;
+	}
+	public Plan getPlanContrato() {
+		return planContrato;
+	}
+	public void setPlanContrato(Plan planContrato) {
+		this.planContrato = planContrato;
+	}
+	
+	
 }
--- AlticeProyectoFinalPOO/src/logico/Pagos.java
+++ AlticeProyectoFinalPOO/src/logico/Pagos.java
@@ -8,7 +8,61 @@ public class Pagos {
 	private Contrato contrato;
 	private LocalDate fechaInicioPago;
 	private LocalDate fechaVencimientoPago;
-	private boolean estadoPagado; //Boolean que si es falso indica que falta por pagar, y si es verdadero es que ha sido pagado totalmente
+	private boolean isPagado; //Boolean que si es falso indica que falta por pagar, y si es verdadero es que ha sido pagado totalmente
 	private float totalPorPagar;
 	private Plan planPorpagar;
+	
+	public Pagos(String idPago, Contrato contrato, LocalDate fechaInicioPago, LocalDate fechaVencimientoPago,float totalPorPagar, Plan planPorpagar) {
+		super();
+		this.idPago = idPago;
+		this.contrato = contrato;
+		this.fechaInicioPago = fechaInicioPago;
+		this.fechaVencimientoPago = fechaVencimientoPago;
+		this.isPagado = false;
+		this.totalPorPagar = totalPorPagar;
+		this.planPorpagar = planPorpagar;
+	}
+	
+	public String getIdPago() {
+		return idPago;
+	}
+	public void setIdPago(String idPago) {
+		this.idPago = idPago;
+	}
+	public Contrato getContrato() {
+		return contrato;
+	}
+	public void setContrato(Contrato contrato) {
+		this.contrato = contrato;
+	}
+	public LocalDate getFechaInicioPago() {
+		return fechaInicioPago;
+	}
+	public void setFechaInicioPago(LocalDate fechaInicioPago) {
+		this.fechaInicioPago = fechaInicioPago;
+	}
+	public LocalDate getFechaVencimientoPago() {
+		return fechaVencimientoPago;
+	}
+	public void setFechaVencimientoPago(LocalDate fechaVencimientoPago) {
+		this.fechaVencimientoPago = fechaVencimientoPago;
+	}
+	public boolean isEstadoPagado() {
+		return isPagado;
+	}
+	public void setEstadoPagado(boolean isPagado) {
+		this.isPagado = isPagado;
+	}
+	public float getTotalPorPagar() {
+		return totalPorPagar;
+	}
+	public void setTotalPorPagar(float totalPorPagar) {
+		this.totalPorPagar = totalPorPagar;
+	}
+	public Plan getPlanPorpagar() {
+		return planPorpagar;
+	}
+	public void setPlanPorpagar(Plan planPorpagar) {
+		this.planPorpagar = planPorpagar;
+	}
 }
